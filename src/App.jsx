cd8cb7dec49f11c810b0d61eb4203236f8a05cde
bikokwa <34458781+bikokwa@users.noreply.github.com>
2024-08-16 16:49:23 +0300

    infinite SWAPI set up

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./App.css";
+import { InfinitePeople } from "./people/InfinitePeople";
+import { InfiniteSpecies } from "./species/InfiniteSpecies";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+function App() {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Infinite SWAPI</h1>
+        <InfinitePeople />
+        {/* <InfiniteSpecies /> */}
+      </div>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
